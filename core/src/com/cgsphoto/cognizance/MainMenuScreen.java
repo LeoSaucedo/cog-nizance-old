d0742c1d9ed4fda8f653d34d9b0ff471f4c9b442
Carlos Saucedo <carlos@cgsphoto.com>
2018-08-07 08:31:55 -0500

    Added initial menu buttons.

--- core/src/com/cgsphoto/cognizance/MainMenuScreen.java
+++ core/src/com/cgsphoto/cognizance/MainMenuScreen.java
@@ -1,15 +1,19 @@
 package com.cgsphoto.cognizance;
 
 import com.badlogic.gdx.Screen;
+
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.audio.Sound;
@@ -19,23 +23,55 @@ public class MainMenuScreen implements Screen {
 	Texture img;
 	Music introMusic;
 	SpriteBatch batch;
-	Skin skin;
 	BitmapFont font;
+	private Stage menuStage;
 
 	
 	//Constructor
 	public MainMenuScreen(final MainGame game) {
+		menuStage = new Stage(new ScreenViewport());
+		Gdx.input.setInputProcessor(menuStage);
 	}
 	
 	@Override
 	public void show() {
-		//Splash screen image
+		// Splash screen image
 		batch = new SpriteBatch();
 		img = new Texture("graphics/splash_screens/start-menu.png");
 		
-		//Intro Music
+		// Intro Music
 		introMusic = Gdx.audio.newMusic(Gdx.files.internal("music/disintegrating.mp3"));
 		introMusic.play();
+		
+		// Menu Options
+		Table menuTable = new Table();
+		menuTable.setFillParent(true);
+		menuTable.setDebug(true);
+		menuStage.addActor(menuTable);
+		
+		// Temporary until we have asset manager in
+		Skin menuSkin = new Skin(Gdx.files.internal("graphics/skins/flat/skin/skin.json"));
+		
+		// Create buttons
+		TextButton newGameButton = new TextButton("New Game", menuSkin);
+		TextButton loadGameButton = new TextButton("Load Game", menuSkin);
+		TextButton exitButton = new TextButton("Exit", menuSkin);
+		
+		// Add buttons to menuTable
+		menuTable.add(newGameButton).fillX().uniformX();
+		menuTable.row().pad(10, 0, 10, 0);
+		menuTable.add(loadGameButton).fillX().uniformX();
+		menuTable.row();
+		menuTable.add(exitButton).fillX().uniformX();
+		
+		// Create Button listeners
+		newGameButton.addListener(new ChangeListener() {
+			@Override
+			public void changed(ChangeEvent event, Actor actor) {
+				System.out.println("User clicked new game button.");
+			}
+		});
+		
 	}
 
 	@Override
@@ -45,6 +81,8 @@ public class MainMenuScreen implements Screen {
 		batch.begin();
 		batch.draw(img, 0, 0);
 		batch.end();
+		
+		menuStage.draw();
 	}
 
 	@Override
